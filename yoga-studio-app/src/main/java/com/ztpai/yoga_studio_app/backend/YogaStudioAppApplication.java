f130d7717cc70fb5cf3153b90d470c8c64d2adb2
KatarzynaTopor <katarzyna.topor@student.pk.edu.pl>
2025-03-02 11:26:39 +0100

    Initial project setup backend and frontend

---
+++ yoga-studio-app/src/main/java/com/ztpai/yoga_studio_app/backend/YogaStudioAppApplication.java
@@ -0,0 +1,13 @@
+package com.ztpai.yoga_studio_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogaStudioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogaStudioAppApplication.class, args);
+	}
+
+}
---
+++ yoga-studio-app/src/test/java/com/ztpai/yoga_studio_app/YogaStudioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ztpai.yoga_studio_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogaStudioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
